2cf597e79d1ba02012e50c4b0d716c55cbb41151
prise <prise@localhost.localdomain>
2019-09-23 15:21:15 +0800

    Removing pyc files

--- app.py
+++ app.py
@@ -19,9 +19,22 @@
 # OOP
 # Python Package -- models
 from database import Database
-from models.post import Post
+# from models.post import Post
+from models.blog import Blog
 
 Database.initialize()
+
+blog = Blog(author="Tito Sotto",
+            title="Tito Sotto Title Sample",
+            description="Sample description")
+
+blog.new_post()
+
+blog.save_to_mongo()
+
+from_database = Blog.from_mongo(blog.id)
+
+print(blog.get_posts())
 
 # post = Post(blog_id='12039847',
 #             title='Guide to sleeping very well',
@@ -32,9 +45,9 @@ Database.initialize()
 # post = Post.from_mongo('65bebe4d83004f1c8030bf2c521655ed')
 # print(post)
 
-posts = Post.from_blog('12039847')
-for post in posts:
-  print(post)
+# posts = Post.from_blog('12039847')
+# for post in posts:
+#   print(post)
 
 # post = Post("Post1 Title", "Post1 content", "Post1 author")
 # post2 = Post("Post2 Title", "Post2 content", "Post2 author")
